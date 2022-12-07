df6d4579aeca66e42cab7a18b1c48edf868b0774
leonsaja <leonardosaja87@gmail.com>
2022-12-07 18:35:48 -0300

    criacao de view e de um template recipe-view

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'recipes/pages/home.html',context={'nome':'Olá Mundo !!!'})
+
+
+
+
+def recipe(request, id):
+    return render (request, 'recipes/pages/recipe-view.html', context=
+    {'recipe': 'Café da amanha'})
+    
