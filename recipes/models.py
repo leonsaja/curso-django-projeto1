9b217df15e6deba5153c5cba332c62c5fe1e1301
leonsaja <leonardosaja87@gmail.com>
2022-12-08 11:36:44 -0300

    criacao dos modelos recipe e category

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=65)
+
+class Recipe (models.Model):
+    title= models.CharField(max_length=65)
+    description=models.CharField(max_length=165)
+    slug=models.SlugField()
+    preparation_time=models.IntegerField()
+    preparation_time_unit=models.CharField(max_length=65)
+    servings=models.IntegerField()
+    servings_unit=models.CharField(max_length=65)
+    prepation_steps = models.TextField()
+    prepation_steps_is_html= models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    is_published=models.BooleanField(default=False)
+    cover=models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    Category=models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
+    User=models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
