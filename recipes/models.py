03c5eba7fdf07cfc744d3229d508997f1f6fde0b
leonsaja <leonardosaja87@gmail.com>
2022-12-08 11:31:01 -0300

    Revert "criacao dos modelos recipe, category e migrate"
    This reverts commit 9b1463921a826f9c2f0a1f7fd67219a1b475e07e.

--- recipes/models.py
+++ recipes/models.py
@@ -1,23 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
-
-class Category(models.Model):
-    name=models.CharField(max_length=65)
-
-class Recipe (models.Model):
-    title= models.CharField(max_length=65)
-    description=models.CharField(max_length=165)
-    slug=models.SlugField()
-    preparation_time=models.IntegerField()
-    preparation_time_unit=models.CharField(max_length=65)
-    servings=models.IntegerField()
-    servings_unit=models.CharField(max_length=65)
-    prepation_steps = models.TextField()
-    prepation_steps_is_html= models.BooleanField(default=False)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-    is_published=models.BooleanField(default=False)
-    cover=models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    Category=models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
-    User=models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+# Create your models here.
