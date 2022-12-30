b4d9fef9fa6b42a1981ea5a0ee8147a54af86ab2
leonsaja <leonardosaja87@gmail.com>
2022-12-30 18:54:08 -0300

    habilitando o campo de busca

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.db.models import Q
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from utils.recipes.factory import make_recipe
@@ -12,6 +13,23 @@ def home(request):
         "recipes":recipes
     })
 
+
+def search(request):
+    search_term=request.GET.get('q','').strip()
+    
+    if not search_term:
+        raise Http404()
+
+    recipes=Recipe.objects.filter((
+    Q(title__icontains=search_term) |
+    Q(description__icontains=search_term)
+    ),is_published=True)
+
+    return render(request, 'recipes/pages/search.html', context={
+        'recipes':recipes,
+        'search':search_term,
+    })
+
 def category(request,category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(
@@ -22,7 +40,7 @@ def category(request,category_id):
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        # 'title': f'{recipes.first().category.name} - Category | '
+        # 'title': f'{recipes.first().category.name} | Category'
         'title': f'{recipes[0].category.name} - Category | '
     })
 
@@ -33,7 +51,6 @@ def recipe(request, id):
     ).order_by('-id').first()
 
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
         'recipe': recipe,
         'is_detail_page': True,
     })
